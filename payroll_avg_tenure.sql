238774ebbf3ef08d43e5ede6f368ce2613df09c1
HubbBuns <80090757+HubbBuns@users.noreply.github.com>
2021-08-09 11:56:08 -0400

    Update payroll_avg_tenure.sql

--- payroll_avg_tenure.sql
+++ payroll_avg_tenure.sql
@@ -3,10 +3,10 @@ USE employees;
 SELECT      
     ROUND(AVG(s.salary), 0) AS salary,	#Getting the average salary, then rounding it to a whole number
     t.title AS title,   #Selecting job titles
-		ROUND(AVG(CASE    #Using the case statement to get the average tenure, rounded to a whole number
+		ROUND(AVG(CASE		#Using the case statement to get the average tenure, rounded to a whole number
 			WHEN YEAR(t.to_date) = 9999 THEN TIMESTAMPDIFF(year, t.from_date, '2002-12-31')	    #If the year of to_date = 9999, then the employee is currently active.  
-			ELSE TIMESTAMPDIFF(year, t.from_date, t.to_date)  									#Instead of using current_now(), which is 2021, 
-		END), 0) AS avg_tenure																	#I used 2002 since thats the latest year of to_date and matches closely to the time period of the database.
+			ELSE TIMESTAMPDIFF(year, t.from_date, t.to_date)  				    #Instead of using current_now(), which is 2021, 
+		END), 0) AS avg_tenure									    #I used 2002 since thats the latest year of to_date and matches closely to the time period of the database.
 FROM titles t 	#Joining the two relevant tables needed, titles and salaries
 JOIN salaries s
 	USING (emp_no)	#Joining the two tables and their primary keys
