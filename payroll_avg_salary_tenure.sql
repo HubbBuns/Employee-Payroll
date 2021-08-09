cc503a0beffbf23616a21de3c597f927fd39b775
HubbBuns <80090757+HubbBuns@users.noreply.github.com>
2021-08-09 14:42:58 -0400

    Update payroll_avg_salary_tenure.sql

--- payroll_avg_salary_tenure.sql
+++ payroll_avg_salary_tenure.sql
@@ -3,7 +3,7 @@ USE employees;
 SELECT      
     ROUND(AVG(s.salary), 0) AS salary,	#Getting the average salary, then rounding it to a whole number
     t.title AS title,   #Selecting job titles
-		ROUND(AVG(CASE    #Using the case statement to get the average tenure, rounded to a whole number
+		ROUND(AVG(CASE    #Using the case statement to get the average tenure, rounded to a whole number, and the year difference from their starting to ending/current date
 			WHEN YEAR(t.to_date) = 9999 THEN TIMESTAMPDIFF(year, t.from_date, '2002-12-31')	    #If the year of to_date = 9999, then the employee is currently active.  
 			ELSE TIMESTAMPDIFF(year, t.from_date, t.to_date)  				    #Instead of using current_now(), which is 2021, 
 		END), 0) AS avg_tenure									    #2002 was used since thats the latest year of to_date and matches closely to the time period of the table.
